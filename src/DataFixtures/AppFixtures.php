75817c45d8354e3efe8b3bb859a2bb7157953e07
Cheezuts <telliercyril@gmail.com>
2023-03-15 14:24:39 +0100

    Setting up login form (0 error but didn't work (why ?) )

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,8 +6,10 @@ use Faker\Factory;
 use Faker\Generator;
 use App\Entity\Recipe;
 use App\Entity\Ingredient;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -47,6 +49,20 @@ class AppFixtures extends Fixture
                     $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) -1)]);
                 }
                 $manager->persist($recipe);
+        }
+
+        // Users
+        for ($i=0; $i < 10; $i++) { 
+            $user = new User();
+            $user->setFullName($this->faker->name())
+            ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
+            ->setEmail($this->faker->email())
+            ->setRoles(['ROLE_USER'])
+            ->setPlainPassword('password');        
+
+            
+
+            $manager->persist($user);
         }
 
             
